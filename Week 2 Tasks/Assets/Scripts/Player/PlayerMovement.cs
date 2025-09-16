35f6a7f27724857e634c7eed5e3eefab7a57b422
Vasanth2801 <vasanthravichandran28@gmail.com>
2025-09-16 08:40:17 +0530

    Cinemachine for Player

---
+++ Week 2 Tasks/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;              // Movement of the Player
+    Rigidbody2D rb;                                        // Rigidbody References
+    Vector2 movement;                                     // Movement for two axis 
+    TopdownController controller;                         //Refernce for InputActions
+
+    //Awake Calls before Start Method
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        controller = new TopdownController();
+
+        MovementCalling();                // A method to call the movement
+    }
+
+    //Method to Handle movement
+    void MovementCalling()
+    {
+        controller.Player.Move.performed += ctx => movement = ctx.ReadValue<Vector2>();     //To read the value when button is pressed 
+        controller.Player.Move.canceled += ctx => movement = Vector2.zero;                  //To read the value when it is canceled 
+    }
+
+
+    private void OnEnable()
+    {
+        controller.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controller.Player.Disable();
+    }
+
+    // Used for physics intervals
+    void FixedUpdate()
+    {
+        HandleMovement();            //Method to Handle movement
+    }
+
+    //Handling the Movement
+    void HandleMovement()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);     //For Moving the Player
+    }
+}
