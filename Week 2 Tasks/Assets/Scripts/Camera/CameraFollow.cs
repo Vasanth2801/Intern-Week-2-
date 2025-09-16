f86b60b9a966160ad070a2f70844a153ee3cec45
Vasanth2801 <vasanthravichandran28@gmail.com>
2025-09-16 12:05:58 +0530

    Smoothing manually

--- Week 2 Tasks/Assets/Scripts/Camera/CameraFollow.cs
+++ Week 2 Tasks/Assets/Scripts/Camera/CameraFollow.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform player;
-    public Vector3 offset;
-    
+    public Transform player;                          //Reference to the player
+    public Vector3 offset;                           // the values for camera to look at a distance or camera offset
+    [SerializeField] private float smoothSpeed = 0.25f;       //smoohting of the camera to follow
+    Vector3 currentVelocity;                              // local velocity movement 
 
+    //Late Update is called after all functions
     private void LateUpdate()
     {
-        offset = new Vector3(0, 0, -10);
-        transform.position = player.transform.position + offset;
+        MoveCamera();                    //Method to call Camera movement
+    }
+
+    // Method for the player movement
+    void MoveCamera()
+    {
+        offset = new Vector3(0, 0, -10);          //offset values for the camera
+        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + offset, ref currentVelocity, smoothSpeed);   //Mathimatical method for the Smoothing function method
     }
 }
