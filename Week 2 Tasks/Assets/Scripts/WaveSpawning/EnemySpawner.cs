9418ffd4d70b8657972ac713a4e219f3d4aff12b
Vasanth2801 <vasanthravichandran28@gmail.com>
2025-09-19 08:32:59 +0530

    Enemy Spawning at interval

--- Week 2 Tasks/Assets/Scripts/WaveSpawning/EnemySpawner.cs
+++ Week 2 Tasks/Assets/Scripts/WaveSpawning/EnemySpawner.cs
@@ -2,5 +2,23 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    
+    [SerializeField] private float countDown = 4f;            //Time interval for spawning the waves 
+
+
+    private void Update()
+    {
+        countDown -= Time.deltaTime;                  //Decreasing the countdown time when the game start running
+
+        if(countDown <= 0f)
+        {
+            //Spawn the Wave 
+            SpawnWave();
+        }
+    }
+
+    void SpawnWave()
+    {
+        Debug.Log("Wave Spawned");                    //Debugging the wave spawning 
+        countDown = 4f;                                //Resetting the countdown again for spawning the next wave
+    }
 }
